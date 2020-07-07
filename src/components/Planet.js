5f5b66cb93e2ab8044df34ebbdde3641c9e16e25
MrLag <emo30000@gmail.com>
2020-07-07 16:11:37 -0500

    [feat] index: Basic model of observers is ready.

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -32,6 +32,7 @@ const Planet = () => {
     <Img
       fluid={data.placeholderImage.childImageSharp.fluid}
       className="planet"
+      id="planet"
     />
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
 import fetchNasa from "../components/fetchNasa"
 
@@ -11,13 +11,31 @@ export default function IndexPage() {
     "https://api.nasa.gov/planetary/apod?api_key=8aYlfZeSF5ubAbeEcQDHSRCO3XQMjkdRmWRO3mdP"
   var data = fetchNasa(API_URL)
   console.log(data)
+  useEffect(() => {
+    const target = document.getElementById("title")
+
+    var options = {
+      rootMargin: "0px",
+      threshold: 1.0,
+    }
+
+    function callback(entries, observer) {
+      console.log("callback called :)")
+    }
+
+    const observer = new IntersectionObserver(callback, options)
+
+    observer.observe(target)
+  }, [])
 
   return (
     <Layout>
       <SEO title="Home" />
       <section className="home grid w-full h-fullvh pl-16 py-40 grid-cols-2 grid-rows-1">
         <div className="intro grid col-start-1 col-end-2 content-center">
-          <div className="intro-title text-3xl font-bold">NASA Daily Image</div>
+          <div id="title" className="intro-title text-3xl font-bold">
+            NASA Daily Image
+          </div>
           <div className="intro-subtitle">Visit every day</div>
         </div>
         <div className="image flex align-middle image--right col-start-2 col-end-3">
