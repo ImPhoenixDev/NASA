d27308608b821aca8114a786a3aa85976e86a574
MrLag <emo30000@gmail.com>
2020-07-14 18:39:01 -0500

    [stylus] About: Responsive upgrade.

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
       <h2 className="about__title self-center row-start-1 row-end-2 col-start-1 col-end-2 text-center justify-center text-5xl">
         About
       </h2>
-      <p className="about__description row-start-2 row-end-3 sm:col-start-2 sm:col-end-3 px-10">
+      <p className="about__description row-start-2 row-end-3 sm:row-start-1 cm:row-end-2 sm:col-start-2 sm:col-end-3 px-10">
         On this page you can find a <strong>diferent image every day </strong>
         provided by <strong>NASA </strong>
         along with a description of it.
